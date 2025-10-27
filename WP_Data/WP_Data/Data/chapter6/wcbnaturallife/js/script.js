f4381699fafc63e72f42d3df59aa55bc74d877b4
kyosuke0530 <artis.kyosuke0530@gmail.com>
2025-10-27 15:20:11 +0900

    first commit

--- WP_Data/WP_Data/Data/chapter6/wcbnaturallife/js/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.btn-menu');
-const nav = document.querySelector('.menu-wrapper');
-
-btn.addEventListener('click', () => {
-  nav.classList.toggle('open-menu')
-  btn.innerHTML = btn.innerHTML === 'メニュー'
-    ? '閉じる'
-    : 'メニュー'
-});
--- vue_nippo/vue-lesson/src/App.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-import { ref } from 'vue'
-</script>
-<template>
-  <div :style="{color: 'red', backgroundColor: 'blue' }">Hello</div>
-</template>
